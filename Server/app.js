9f4cf16474ef848cc4ff9bd5e4a226c050384834
Gowtham MA <magowtham7@gmail.com>
2023-11-14 15:19:06 +0530

    testing

--- Server/app.js
+++ Server/app.js
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 5000;
 //mongodb connection
 connectDB();
 //middlewares
+ app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", req.headers.origin);
+   res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   next();
+ }); 
 app.use(cookieParser());
 app.use(express.json());
   app.use(
